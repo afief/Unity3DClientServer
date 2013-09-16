371e1750281a244481b666d84b23715bba698300
Afief <ramacyber@gmail.com>
2013-09-17 00:10:05 +0700

    ClientServer di Unity3D
    Server side untuk membaca data data yang masuk dari klien side

---
+++ clientside.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections;
+
+
+public class clientside : MonoBehaviour {
+
+	private TcpClient client;
+	private NetworkStream stream;
+	private StreamReader reader;
+	private StreamWriter writer;
+	
+	private String host = "200.200.200.40";
+	private int port = 83;
+
+	// Use this for initialization
+	void Start () {
+		client = (TcpClient) new TcpClient(host, port);
+		stream = client.GetStream();
+		reader = new StreamReader(stream);
+		writer = new StreamWriter(stream);
+	}
+	void writeSocket(String teks) {
+		writer.Write(teks + "\n\r");
+		writer.Flush();
+	}
+	void readSocket() {
+		if (stream.CanRead && stream.DataAvailable) {
+			Debug.Log(reader.ReadLine());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate() {
+		readSocket();
+		writeSocket("kompi-" + Time.deltaTime.ToString());
+	}
+}
---
+++ serverside.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class serverside : MonoBehaviour {
+	
+	private TcpListener server;
+	
+	public String host = "200.200.200.40";
+	public int port = 83;
+	
+	private List<NetworkStream> netStreams = new List<NetworkStream>();
+	
+	private IPAddress ipaddress;
+
+	// Use this for initialization
+	void Start () {
+		ipaddress = IPAddress.Parse(host);
+		
+		server = (TcpListener) new TcpListener(ipaddress, port);
+		server.Start();
+		
+		server.BeginAcceptTcpClient(new AsyncCallback(tcpClientMasuk), server);
+	}
+	void tcpClientMasuk(IAsyncResult ar) {
+		TcpListener _tcpListener = (TcpListener) ar.AsyncState;
+		TcpClient _client = _tcpListener.EndAcceptTcpClient(ar);
+		
+		netStreams.Add(_client.GetStream());
+
+		Debug.Log("Connected");
+	}
+	
+	void FixedUpdate() {
+		for (int i = 0; i < netStreams.Count; i++) {
+			readStream(netStreams[i]);
+		}
+	}
+	void readStream(NetworkStream ns) {
+		StreamReader _reader = new StreamReader(ns);
+		while (ns.CanRead && ns.DataAvailable) {
+			Debug.Log(_reader.ReadLine());
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
